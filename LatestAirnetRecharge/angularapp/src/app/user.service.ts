d1a73b252b09b8c49b1eb60e0abd5eb6546c2822
NikhilMurugesan <49281792+NikhilMurugesan@users.noreply.github.com>
2022-08-10 14:32:58 +0530

    first commit

---
+++ LatestAirnetRecharge/angularapp/src/app/user.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  isValid() {
+      throw new Error('Method not implemented.');
+  }
+  updateUser: any;
+  getUserbyid: any;
+  getUserById(id: number) {
+    throw new Error('Method not implemented.');
+  }
+  private baseURL = "http://localhost:8080/api/v1/Users";
+  private baseURL1 = "http://localhost:8080/api/v1/Users/email";
+  constructor(private httpClient:HttpClient) { }
+
+  public LoginUserFromRemote(user:User): Observable<any>{
+
+    return this.httpClient.post<any>("http://localhost:8080/login",user);
+  }
+
+
+  public authentication(user:User): Observable<any>{
+    return this.httpClient.get<any>("http://localhost:8080/userrole");
+
+
+  }
+
+  getUserList(): Observable<User[]>
+  {
+    return this.httpClient.get<User[]>(`${this.baseURL}`);
+  }
+  createUser(user:User):Observable<Object>
+  {
+    return this.httpClient.post(`${this.baseURL}`,user);
+  }
+  getUsersById(id: number) :Observable<User>
+  {
+    return this.httpClient.get<User> (`${this.baseURL}/${id}`);
+  }
+  edituser(id: number, user: User): Observable<Object>{
+    return this.httpClient.put(`${this.baseURL}/${id}`, user);
+  }
+  getUserbyemailId(emailId : String) : Observable <User>
+  {
+    return this.httpClient.get<User> (`${this.baseURL1}/${emailId}`);
+  }
+
+  deleteuser(id: number): Observable<Object>{
+    return this.httpClient.delete(`${this.baseURL}/${id}`);
+  }
+
+}
